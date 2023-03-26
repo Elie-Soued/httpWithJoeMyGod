53882ca6dfeb3f0f273059ac040274ed52b94f69
Elie Soued <eliegebransoued@gmail.com>
2023-03-26 18:30:13 +0300

    remove cors package for now

--- Servers/express.js
+++ Servers/express.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 
 //Constants declarations
 const app = express();
@@ -8,7 +7,6 @@ const port = 5000;
 //Use Body Parser to format the body´s reponse
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (_, res) => res.send('Hello World'));
 
