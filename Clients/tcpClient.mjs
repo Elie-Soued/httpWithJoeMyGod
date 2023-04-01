29cab7a3fb0d10accdeddf982add268df680626f
Elie Soued <eliegebransoued@gmail.com>
2023-04-02 00:45:37 +0300

    creating clients with the net module

--- Clients/tcpClient.mjs
+++ Clients/tcpClient.mjs
@@ -1,7 +1,12 @@
+/** To run the file open your terminal and run node tcpCient.mjs
+ * You can only run one command at a time.
+ * If you run the command as is you will get all the todos.
+ * To create a post , comment out the first command and uncomment the second command.
+ */
+
 import { createConnection } from 'net';
 
 //Create the sendRequest Function
-
 const sendRequest = function (method, path, callback, body = null) {
     let response = '';
     const socket = createConnection(this.port, this.host);
@@ -24,9 +29,6 @@ const sendRequest = function (method, path, callback, body = null) {
 
     socket.on('data', (chuck) => {
         response += chuck;
-    });
-
-    socket.on('end', () => {
         const [headers, body] = response.split('\r\n\r\n');
         const statusCode = headers.split(' ')[1];
         callback(null, {
@@ -41,8 +43,8 @@ const sendRequest = function (method, path, callback, body = null) {
     });
 };
 
-//Create a factory function that will return  a client Object with a sendRequest function as a method
-
+//Create a factory function that will return  a client Object
+// with a sendRequest function as a method
 const CreateHttpClient = (port, host) => {
     const client = {};
     client.port = port;
@@ -53,16 +55,76 @@ const CreateHttpClient = (port, host) => {
 };
 
 // Instantiate the client
+const client = CreateHttpClient(80, 'jsonplaceholder.typicode.com');
 
-const client = CreateHttpClient(5000, 'localhost');
+// Get all todos
+//----------------
 
-// Send the request
+client.sendRequest(
+    'GET',
+    '/todos/',
+    (err, resp) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(resp.body);
+    },
+    null
+);
 
-client.sendRequest('GET', '/', (err, resp) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+// Create a post
+//---------------
 
-    console.log(resp.body);
-});
+// client.sendRequest(
+//     'POST',
+//     '/posts/',
+//     (err, resp) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         console.log(resp.body);
+//     },
+//     JSON.stringify({
+//         title: 'foo5',
+//         body: 'bar5',
+//         userId: 1,
+//     })
+// );
+
+// Updating the todo with id 1
+//----------------------------
+
+// client.sendRequest(
+//     'PUT',
+//     '/todos/',
+//     (err, resp) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         console.log(resp.body);
+//     },
+//     JSON.stringify({
+//         title: 'salut',
+//         body: 'Je suis un fils de ouf',
+//         userId: 1,
+//     })
+// );
+
+// Get todo with id 1
+//----------------
+
+// client.sendRequest(
+//     'GET',
+//     '/todos/1',
+//     (err, resp) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         console.log(resp.body);
+//     },
+//     null
+// );
