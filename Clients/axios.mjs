3fa365f40ae9479714c09b4a6425d0b48b51c635
Elie Soued <eliegebransoued@gmail.com>
2023-04-01 23:30:24 +0300

    creating clients with axios and the http module

--- Clients/axios.mjs
+++ Clients/axios.mjs
@@ -1,12 +1,55 @@
+/** To run the file open your terminal and run node axios.mjs
+ * You can only run one command at a time.
+ * If you run the command as is you will get all the todos.
+ * To get the todo with id 1, comment out the first command and uncomment the second command.
+ */
+
 import axios from 'axios';
 
-const getResponse = async () => {
+const URLTodos = 'https://jsonplaceholder.typicode.com/todos/';
+const URLPosts = 'https://jsonplaceholder.typicode.com/posts/';
+const headers = {
+    'Content-Type': 'application/json;charset=utf-8',
+};
+
+const doRequest = async (method, url, body) => {
     try {
-        const response = await axios('http://localhost:5000/');
+        const response = await axios({
+            method,
+            url,
+            headers,
+            body,
+        });
         console.log(response.data);
     } catch (error) {
-        console.log(error, 'someting happened');
+        console.log(error, 'DB might be out of sync');
     }
 };
 
-getResponse();
+// Get all todos
+//---------------
+doRequest('GET', URLTodos);
+
+// Get  the todo with id 1
+//---------------------------
+// doRequest('GET', `${URLTodos}/1`);
+
+// Create a new Todo
+//-------------------
+// doRequest('POST', URLPosts, {
+//     title: 'foo5',
+//     body: 'bar5',
+//     userId: 1,
+// });
+
+// Updating the todo with id 1
+//----------------------------
+// doRequest('PUT', `${URLPosts}/1`, {
+//     title: 'Joe',
+//     body: 'est un ouf',
+//     userId: 1,
+// });
+
+// Deleting the todo with id 1
+//----------------------------
+// doRequest('DELETE', `${URLPosts}/1`);
--- Clients/httpClient.mjs
+++ Clients/httpClient.mjs
@@ -1,25 +1,96 @@
-import { request } from 'http';
+/** To run the file open your terminal and run node httpCient.mjs
+ * You can only run one command at a time.
+ * If you run the command as is you will create a post.
+ * To get all todos , comment out the first command and uncomment the second command.
+ */
 
-const options = {
-    method: 'GET',
-    hostname: 'localhost',
-    port: 5000,
+import http from 'http';
+
+//Generic function to do a request
+const doRequest = (options, postData) => {
+    const req = http.request(options, (res) => {
+        res.setEncoding('utf8');
+
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    if (postData) {
+        req.write(postData);
+    }
+    req.end();
 };
 
-const req = request(options, (res) => {
-    let response = '';
-
-    res.on('data', (chunk) => {
-        response += chunk;
-    });
-
-    res.on('close', () => {
-        console.log(response);
-    });
-
-    res.on('error', (err) => {
-        console.log(err);
-    });
+// Create a post
+//---------------
+const postData = JSON.stringify({
+    title: 'foo5',
+    body: 'bar5',
+    userId: 1,
 });
 
-req.end();
+const optionsToCreateAPost = {
+    hostname: 'jsonplaceholder.typicode.com',
+    path: '/posts/',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postData),
+    },
+};
+
+doRequest(optionsToCreateAPost, postData);
+
+// Get all todos
+//---------------
+
+const optionsToGetAllTodos = {
+    hostname: 'jsonplaceholder.typicode.com',
+    path: '/todos/',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+// doRequest(optionsToGetAllTodos);
+
+// Updating the todo with id 1
+//----------------------------
+const optionsUpdateTodoId1 = {
+    hostname: 'jsonplaceholder.typicode.com',
+    path: '/todos/1',
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+const updateData = JSON.stringify({
+    title: 'Pilex',
+    body: 'laFlex',
+    userId: 1,
+});
+
+//doRequest(optionsUpdateTodoId1, updateData);
+
+// Delete the todo with id 2
+//----------------------------
+const optionsDeleteTodoId2 = {
+    hostname: 'jsonplaceholder.typicode.com',
+    path: '/todos/2',
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+// doRequest(optionsDeleteTodoId2);
